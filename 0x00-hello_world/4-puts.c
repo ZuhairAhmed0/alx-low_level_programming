d34119817773c4da2ad0fff9a4a50867edcf7033
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-10-14 02:28:26 +0200

    Last update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-/*
- * File: 4-puts.c
- * Auth: Zuhair Ahmed
- */
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
- * Return: Always 0.
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
