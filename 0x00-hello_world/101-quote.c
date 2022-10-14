fa96d702e1ec9cca816c63798ad7cdca3ed96a7b
ZuhairAhmed0 <zuh12390@gmail.com>
2022-10-14 02:26:32 -0700

    last update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 /*
- *  * File: 101-quote.c
- *   * Auth: Zuhair Ahmed
- *    */
-
+ * File: 101-quote.c
+ * Auth: Zuhair Ahmed
+ */
 #include <unistd.h>
-
 /**
  *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+ *                  followed by a new line, to standard error.
+ *   
+ *  Return: Alw */
 int main(void)
 {
 	write(2, 
