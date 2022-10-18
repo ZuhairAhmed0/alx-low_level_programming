1b182e2896315e908a4776a3752abc3e84bcd08d
ZuhairAhmed0 <zuh12390@gmail.com>
2022-10-18 16:49:02 -0700

    last update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result
+ * @a: number one
+ * @b: number two
+ *
+ * File: 10-add.c
+ * Auth: Zuahir Ahmed
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Write a function that prints all natural numbers from n to 98
+ * @n: number start
+ *
+ * File: 11-print_to_98.c
+ * Auth: Zuahir Ahmed
+ *
+ * Return: all number from n to 98
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i >= 98; i++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
