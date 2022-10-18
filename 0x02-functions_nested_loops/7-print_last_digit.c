18b1b598131c3f459554db576811f6b597407741
ZuhairAhmed0 <zuh12390@gmail.com>
2022-10-18 16:15:25 -0700

    last update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,14 @@
  * File: 7-print_last_digit.c
  * Auth: Zuahir Ahmed
  *
- * Return: last digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
 	int digit = n % 10;
 
+	if (digit < 0)
+		digit *= -1;
+	_putchar(digit + '0');
 	return (digit);
 }
