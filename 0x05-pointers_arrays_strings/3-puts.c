d89d45d418fb8d33891bd7c793896cbf48fea23f
ZuhairAhmed0 <zuh12390@gmail.com>
2022-10-24 10:43:14 -0700

    last update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ *
+ * File: 3-puts.c
+ * Auth: Zuhair Ahmed
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
