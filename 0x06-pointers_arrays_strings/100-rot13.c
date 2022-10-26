dd6d666e199c3f40852f1c0f8a9a2ffd1bd319e4
ZuhairAhmed0 <zuh12390@gmail.com>
2022-10-26 12:59:17 -0700

    last update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,15 +13,15 @@ char *rot13(char *ptr)
 {
 	int i = 0;
 
-	while (ptr[i])
+	while (*(ptr + i))
 	{
-		if (ptr[i] >= 'A' && ptr[i] <= 'Z')
+		if (*(ptr + i) >= 'A' && *(ptr + i) <= 'Z')
 		{
-			ptr[i] = 'A' + (ptr[i] - 'A' + 13) % 26;
+			*(ptr + i) = 'A' + (*(ptr + i) - 'A' + 13) % 26;
 		}
-		else if (ptr[i] >= 'a' && ptr[i] <= 'z')
+		else if (*(ptr + i) >= 'a' && *(ptr + i) <= 'z')
 		{
-			ptr[i] = 'a' + (ptr[i] - 'a' + 13) % 26;
+			*(ptr + i) = 'a' + (*(ptr + i) - 'a' + 13) % 26;
 		}
 		i++;
 	}
