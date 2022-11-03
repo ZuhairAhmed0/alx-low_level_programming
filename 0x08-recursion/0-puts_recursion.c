7f1a2fc662309f73739e705218eed75fdd5790c1
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-03 12:51:41 -0700

    last update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string to print
+ *
+ * File: 0-puts_recursion.c
+ * Auth: Zuhair Ahmed
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
