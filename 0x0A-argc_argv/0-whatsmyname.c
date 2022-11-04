e80429d4e9d9bcb9bfaff3f9b22c88ba67fc9810
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-04 06:13:09 -0700

    last update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new lin
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * File: 0-whatsmyname.c
+ * Auth: Zuhair Ahmed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
