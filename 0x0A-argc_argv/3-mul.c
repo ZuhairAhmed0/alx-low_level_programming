239480fde756904b6c07245a00c918266653f8eb
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-04 10:23:05 -0700

    last update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,18 @@
  * File: 3-mul.c
  * Auth: Zuhair Ahmed
  *
- * Return: Always 0 
+ * Return: 0 Success, 1 Error
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 1)
+	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 	else
+	{
 		printf("%s\n", "Error");
-	return (0);
+		return (1);
+	}
+		return (0);
 }
