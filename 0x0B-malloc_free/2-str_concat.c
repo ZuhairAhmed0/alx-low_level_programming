68e7cf6d25419612fab3e66fe00bb4fd53607f45
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-07 11:57:48 -0800

    last update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,13 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i]; i++)
 		len++;
-	for (i - 0; s2[i]; i++)
+	for (i = 0; s2[i]; i++)
 		len++;
 
 	if (len == 0)
 		return (NULL);
 
-	str = malloc(typeof(char) * (len + 1));
+	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
