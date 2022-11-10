734cacd381348e1a615e91c146e16927b6b37cff
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-10 10:57:01 -0800

    last update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - function that concatenates two strings
+ * string_nconcat - function that concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: The maximum number
@@ -11,7 +11,7 @@
  *
  * Return: pointer content s1 and s2 or NULL on failure
  */
-char *str_concat(char *s1, char *s2, unsigned int n)
+char string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i, idx = 0, len = 0;
 	char *str;
