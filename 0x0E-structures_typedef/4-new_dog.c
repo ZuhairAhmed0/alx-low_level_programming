94fcf4288b29ba3e9c39781b03e03d5cd59f8162
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-11-14 22:13:51 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+char *_strcopy(char *dest, char *src);
+
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - function that creates a new dog.
