b6216b5ffc5bc6bf4aca9c026bc74e7b63dac9be
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-11-14 14:15:45 +0200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,9 +4,10 @@
 /*
  * File: 3-function_like_macro.h
  * Auth: Zuhair Ahmed
- *
- * Desc: ABS(x) that computes the absolute value of a number
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
