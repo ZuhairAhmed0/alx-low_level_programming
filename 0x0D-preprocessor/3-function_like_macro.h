3f62e04d527c29883ce72bdcbe2cc14e7dd9586d
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-14 03:48:33 -0800

    last update

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Zuhair Ahmed
+ *
+ * Desc: ABS(x) that computes the absolute value of a number
+ */
+
+#define ABS(x) ((x) > 0 ? (-x) : (x))
+
+#endif
