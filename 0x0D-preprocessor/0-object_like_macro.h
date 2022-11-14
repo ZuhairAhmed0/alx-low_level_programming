929ceb5e25aa6ede361fd68d634925431c64a34d
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-14 03:06:57 -0800

    last update

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,2 +1,12 @@
-// header file that defines a macro named SIZE as an abbreviation for the token 1024
-#define SIZE 1024
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_
+
+/**
+ * File: 0-object_like_macro.h
+ * Auth: Zuhair Ahmed
+ * Desc: header file that defines a macro named
+ *	SIZE as an abbreviation for the token 1024
+ */
+#define SIZE 102
+
+#endif
