5d81aa47c617b7a85dfdc9edff74d1399bffb1ad
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-11-14 22:43:49 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include <sdtio.h>
+#include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from
- *
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  * File: 2-main.c
  * Auth: Zuhair Ahmed
  *
- * Return: Always 0 Success
+ * Return: Always 0.
  */
 int main(void)
 {
