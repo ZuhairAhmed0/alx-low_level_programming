04e6fe75d1ee51391be2c1993fcfe8b421e0f85f
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-14 03:38:33 -0800

    last update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <sdtio.h>
+
+/*
+ * main - prints the name of the file it was compiled from
+ *
+ * File: 2-main.c
+ * Auth: Zuhair Ahmed
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
