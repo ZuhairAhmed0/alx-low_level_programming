f6c1c4b308bcc0002612742eb01524dc811e9809
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-14 12:35:15 -0800

    last update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: the struct dog to be free
+ *
+ * File: 5-free_dog.c
+ * Auth: Zuhair Ahmed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(e->onwer);
+	free(d);
+}
