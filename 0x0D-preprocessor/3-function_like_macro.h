88e20e7533f4e91ec5255d28717737368744063a
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-11-14 14:50:09 +0200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/*
+/**
  * File: 3-function_like_macro.h
  * Auth: Zuhair Ahmed
  * Desc: Header file that defines a function-like macro ABS(x)
