47c59d4c99077b1a2a1fe21ac118e7e200ad86a3
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-14 03:16:08 -0800

    last update

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Zuhair Ahmed
+ *
+ * Desc: Create a header file that defines a macro named PI
+ *	PI as an abbreviation for the token 3.1415926535
+ */
+
+#define PI 3.1415926535
+
+#endif
