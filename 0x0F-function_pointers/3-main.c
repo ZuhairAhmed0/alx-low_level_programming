d407393dcd90feacea466dd07455e44f3a376251
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-18 12:18:33 -0800

    last update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 
 /**
@@ -31,22 +31,22 @@ int main(int argc, char *argv[])
 	two = atoi(argv[3]);
 	get_op = argv[2];
 
-	if (get_op_fun(argv[2]) == NULL || argc[2][1] != '\0')
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*get_op == '/' || *get_op == '%') & (*argv[3] == '0'))
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	res = get_op_fun(grt_op);
+	res = get_op_func(get_op);
 	ans = res(one, two);
 
-	printf("%d\n," ans);
+	printf("%d\n", ans);
 	return (0);
 }
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include <sdtio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
