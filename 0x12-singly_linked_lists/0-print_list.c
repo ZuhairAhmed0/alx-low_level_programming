a8d4aa957929c531bc84fdd1a1f5f838d71f22c5
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-24 13:35:19 -0800

    last update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_list - function that prints all the elements of a list_t list
- * @list_t: the list to be print
+ * @h: the list to be print
  *
  * File: 0-print_list.c
  * Auth: Zuhair Ahmed
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	while (h 	!= NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
