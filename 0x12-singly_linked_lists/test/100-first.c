1343b985ef8fa18320eee7b5bc54ce64b14a0981
ZuhairAhmed0 <zuh12390@gmail.com>
2022-11-24 15:16:50 -0800

    last update

---
+++ 0x12-singly_linked_lists/test/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) print_before(void);
+/**
+ * print_before - function that prints before 
+ *	the main function is executed.
+ *
+ * File: 100-first.c
+ * Auth: Zuhair Ahmed
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
