ad5d37725b10b54ef76c3490f2f7bf6b6c9547ec
ZuhairAhmed0 <zuh12390@gmail.com>
2023-02-03 04:50:34 -0800

    message

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_t = malloc(sizeof(hash_table_t) * size);
 	if (hash_t == NULL)
 		return (NULL);
-	
+
 	hash_t->size = size;
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t->array == NULL)
