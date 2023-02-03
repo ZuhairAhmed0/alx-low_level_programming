1567839829d459d1acc8c77ef53945d0ba227d50
ZuhairAhmed0 <zuh12390@gmail.com>
2023-02-03 04:01:45 -0800

    message

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
+	unsigned long int i = 0;
+
+	if (size < 1)
+		return (NULL);
 
 	hash_t = malloc(sizeof(hash_table_t) * size);
 	if (hash_t == NULL)
 		return (NULL);
 
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_t->array == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < size; i++)
+		hash_t->array[i] = NULL;
+
+	hash_t->size = size;
 	return (hash_t);
 }
