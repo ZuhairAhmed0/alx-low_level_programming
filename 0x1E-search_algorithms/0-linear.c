61e5273a2f6508681e3a8a5f582f26775e89eb6e
ZuhairAhmed0 <zuh12390@mail.com>
2023-07-12 07:55:15 -0700

    betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
-* linear_search - function that searches for a value in an array of integers 
+* linear_search - function that searches for a value in an array of integers
 *       using the Linear search algorithm
 *
 * @array: is a pointer to the first element of the array to search in
@@ -10,19 +10,18 @@
 * Return: first index where value is located
 *       if value is not present in array or if array is NULL return -1
 */
-
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if(array[i] == value)
-            return i;
-    }
-    return -1;
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,8 +1,7 @@
 #include "search_algos.h"
-
 /**
-* binary_search - function that searches for a value in a sorted array of integers
-*       using the Binary search algorithm
+* binary_search - function that searches for a value in a sorted
+*	arrayl of integers using the Binary search algorithm
 *
 * @array: is a pointer to the first element of the array to search in
 * @size: is the number of elements in array
@@ -11,47 +10,43 @@
 * Return: first index where value is located
 *       if value is not present i   n array or if array is NULL return -1
 */
-int helper(int *array, size_t first, size_t last, int value);
-
 int binary_search(int *array, size_t size, int value)
 {
-
-    if (array == NULL)
-        return (-1);
-
-    return (helper(array, 0, size - 1, value));
+	if (array == NULL)
+		return (-1);
+	return (helper(array, 0, size - 1, value));
 }
-
 /**
 * helper - array helper
-* @array: array
+* @array: array yo be search
 * @first: first element
 * @last: last element
-* @value: value
+* @value: value to search
 *
 * Return: index of value or -1
 */
 int helper(int *array, size_t first, size_t last, int value)
 {
-    size_t i = first, mid;
+	size_t i = first, mid;
 
-    if (first > last)
-        return (-1);
-    printf("Searching in array: %d", array[i++]);
-    while (i <= last)
-        printf(", %d", array[i++]);
-    printf("\n");
+	if (first > last)
+		return (-1);
 
-    mid = first + ((last - first) / 2);
-    if (array[mid] == value)
-        return (mid);
-    else if (array[mid] > value)
-    {
-        if (mid != 0)
-            return (helper(array, first, mid - 1, value));
-        else
-            return (-1);
-    }
-    else
-        return (helper(array, mid + 1, last, value));
+	printf("Searching in array: %d", array[i++]);
+	while (i <= last)
+		printf(", %d", array[i++]);
+	printf("\n");
+	mid = first + ((last - first) / 2);
+
+	if (array[mid] == value)
+		return (mid);
+	else if (array[mid] > value)
+	{
+		if (mid != 0)
+			return (helper(array, first, mid - 1, value));
+		else
+			return (-1);
+	}
+	else
+		return (helper(array, mid + 1, last, value));
 }
